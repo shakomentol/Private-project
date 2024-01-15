8d4d7036f66bd8d2fe72ae65befcec7c80101088
shakomentol <shakometonidze@gmail.com>
2024-01-15 16:31:46 +0400

    add portfolio page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimationPageComponent } from './components/animation-page/animation-page.component';
 import { MenuPageComponent } from './components/menu-page/menu-page.component';
+import { PortfolioPageComponent } from './components/portfolio-page/portfolio-page.component';
 
 const routes: Routes = [
   { path: 'menu', component: MenuPageComponent },
   { path: 'animation', component: AnimationPageComponent },
+  { path: 'portfolio', component: PortfolioPageComponent },
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
